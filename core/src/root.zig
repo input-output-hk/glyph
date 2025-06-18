cbab355f6a0e4f1f7907a7668bdc35c14f721482
microproofs <kwhitemsg@gmail.com>
2025-06-17 20:04:33 -0400

    chore: frames and heaps and values

--- core/src/root.zig
+++ core/src/root.zig
@@ -7,7 +7,6 @@ const testing = std.testing;
 pub export fn init() u32 {
     const initial_term_addr: u32 = 0x90000000;
     const ptr: *const Term = @ptrFromInt(initial_term_addr);
-
     const x = @intFromPtr(ptr) + 4;
 
     return x;
@@ -54,6 +53,46 @@ test "apply functionality" {
 
     try testing.expect(funcVar == 1);
     try testing.expect(argVar == 2);
+}
+
+test "heap functionality" {
+    var heap = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer heap.deinit();
+    const heapMemory = try heap.allocator().alloc(u32, 10000);
+    const heapPointer: [*]u8 = @ptrCast(heapMemory);
+    var myHeap = Heap{ .heap_ptr = heapPointer };
+    const env: ?*Env = null;
+
+    const argument: []const u32 = &.{ 1, 0, 2 };
+    const argPointer: *const u32 = @ptrCast(argument);
+    const thing: u32 = @truncate(@intFromPtr(argPointer));
+    const function: []const u32 = &.{ 3, thing, 2, 0, 1 };
+    const ptr: *const Term = @ptrCast(function);
+
+    const applyStruct = switch (ptr.*) {
+        .apply => ptr.appliedTerms(),
+        else => @panic("NOOOOOOOOOO"),
+    };
+
+    const funcVar = switch (applyStruct.function.*) {
+        .lambda => applyStruct.function.termBody().debruijnIndex(),
+        else => @panic("NOOOOOOOOOO"),
+    };
+
+    const argVar = switch (applyStruct.argument.*) {
+        .delay => myHeap.createDelay(env, applyStruct.argument.termBody()),
+        else => @panic("NOOOOOOOOOO"),
+    };
+
+    const argIndex = switch (argVar.*) {
+        .delay => |other| blk: {
+            break :blk other.body.debruijnIndex();
+        },
+        else => @panic("NO"),
+    };
+
+    try testing.expect(funcVar == 1);
+    try testing.expect(argIndex == 2);
 }
 
 const Term = enum(u32) {
@@ -120,4 +159,86 @@ const TermList = extern struct { length: u32, list: []*Term };
 const DefaultFunction = enum(u32) {
     addInteger,
     subtractInteger,
+};
+
+const Constant = enum {
+    int,
+    bytestring,
+};
+
+const Value = union(enum) {
+    constant: *Constant,
+    delay: struct {
+        env: ?*Env,
+        body: *const Term,
+    },
+    lambda: struct {
+        env: ?*Env,
+        body: *const Term,
+    },
+};
+
+const Frame = union(enum) {
+    frameAwaitArg: struct {
+        function: *const Value,
+    },
+    frameAwaitFunTerm: struct {
+        env: ?*Env,
+        argument: *const Term,
+    },
+    frameAwaitFunValue: struct {
+        argument: *const Value,
+    },
+    frameForce: void,
+    frameConstr: struct {
+        env: ?*Env,
+        tag: u32,
+        fields: TermList,
+    },
+    frameCases: struct {
+        env: ?*Env,
+        branches: TermList,
+    },
+    noFrame: void,
+};
+
+const Env = extern struct {
+    value: *Value,
+    next: ?*Env,
+};
+
+const Heap = struct {
+    heap_ptr: [*]u8,
+
+    // Creates Delay on the heap and returns a pointer to it
+    fn createDelay(heap: *Heap, env: ?*Env, body: *const Term) *Value {
+        var val = Value{ .delay = .{ .env = env, .body = body } };
+
+        const valData: *align(4) [@sizeOf(Value)]u8 = std.mem.asBytes(&val);
+
+        @memcpy(heap.heap_ptr, valData);
+
+        const heapVal: [*]align(4) u8 = @alignCast(heap.heap_ptr);
+        const heapVal2: *Value = @ptrCast(heapVal);
+
+        heap.heap_ptr = heap.heap_ptr + @sizeOf(Value);
+
+        return heapVal2;
+    }
+
+    // Creates Delay on the heap and returns a pointer to it
+    fn createLambda(heap: *Heap, env: ?*Env, body: *const Term) *const Value {
+        var val = Value{ .lambda = .{ .env = env, .body = body } };
+
+        const valData: *align(4) [@sizeOf(Value)]u8 = std.mem.asBytes(&val);
+
+        @memcpy(heap.heap_ptr, valData);
+
+        const heapVal: [*]align(4) u8 = @alignCast(heap.heap_ptr);
+        const heapVal2: *Value = @ptrCast(heapVal);
+
+        heap.heap_ptr = heap.heap_ptr + @sizeOf(Value);
+
+        return heapVal2;
+    }
 };
